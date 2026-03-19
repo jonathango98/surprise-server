3bbab2001b5fd6604e5f798af29e2a27d18e575b
jonathango98 <jonathangoenadibrata@gmail.com>
2026-03-18 17:09:19 -0700

    add s3 tigris stuff

--- src/s3.js
+++ src/s3.js
@@ -3,6 +3,7 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 export const s3 = new S3Client({
   region: process.env.AWS_REGION,
+  endpoint: process.env.S3_ENDPOINT,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
